2ba4b7b84ae5bfae9df632a5c54dfac4e2102343
Jakob Marušič <jm8166@student.uni-lj.si>
2019-04-18 11:18:26 +0200

    CRC dela

---
+++ TIS - naloga3/naloga3.m
@@ -0,0 +1,44 @@
+function [izhod, crc] = naloga3(vhod, n, k)
+  % Izvedemo dekodiranje binarnega niza vhod, ki je bilo
+  % zakodirano s Hammingovim kodom H(n,k)
+  % in poslano po zasumljenem kanalu.
+  % Nad vhodom izracunamo vrednost crc po standardu CRC-16.
+  %
+  % vhod  - binarni vektor y (vrstica tipa double)
+  % n     - stevilo bitov v kodni zamenjavi
+  % k     - stevilo podatkovnih bitov v kodni zamenjavi
+  % crc   - crc vrednost izracunana po CRC-16 
+  %         nad vhodnim vektorjem (sestnajstisko)
+  % izhod - vektor podatkovnih bitov, dekodiranih iz vhoda
+
+  % CRC
+  vhod;
+  gx = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+  size(gx);
+  velikost = size(vhod, 2);
+  %tmp = gx(16)
+  
+  for i = 1:velikost
+    pm = xor(gx(16), vhod(i));
+    p5 = xor(gx(5), pm);
+    p12 = xor(gx(12), pm);
+    
+    gx = circshift(gx, 1);
+    gx(1) = pm;
+    gx(6) = p5;
+    gx(13) = p12;
+    gx;
+  end
+  
+  obrnjena = (1:16);
+  velikost = size(gx,2);
+  for i = 1:velikost
+    obrnjena(i) = gx(velikost+1-i);
+  end
+  obrnjena = int2str(obrnjena);
+  obrnjena = bin2dec(obrnjena);
+  obrnjena = dec2hex(obrnjena);
+  
+  izhod=1;
+  crc = obrnjena
+end
---
+++ TIS - naloga3/test_naloga3.m
@@ -0,0 +1,26 @@
+% Funkcija za preverjanje naloge 3.
+% Primer zagona:
+% test_naloga3('primeri',1);
+
+function success = test_naloga3(caseDir,caseID)
+	  
+	% Nalozi vhodne podatke in resitev
+	caseData = load([caseDir,filesep,num2str(caseID),'.mat']);
+		
+	% Pozeni 
+	tic();
+	[izhod, crc] = naloga3(caseData.vhod, caseData.n, caseData.k);
+	timeElapsed = toc();
+	
+	% Preveri rezultate
+	success1 = isequal(izhod(:), caseData.izhod(:));
+	success2 = isequal(upper(crc(:)), caseData.crc(:));
+	success = success1 * 0.5 + success2 * 0.5;
+    
+	printf('Rezultat za primer %d\n',caseID);
+	printf('-----------------------------\n');	
+	printf('St. tock: %d\n',success);
+	printf('Cas izvajanja: %f sekund.\n',timeElapsed);
+	printf('-----------------------------\n');
+
+end
